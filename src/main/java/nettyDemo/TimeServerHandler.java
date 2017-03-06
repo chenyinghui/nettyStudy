f2af062a45f9f824d783ed68d2fd4c28a19c49e1
Administrator <Administrator@windows10.microdone.cn>
2017-03-06 10:29:45 +0800

    modify

--- src/main/java/nettyDemo/TimeServerHandler.java
+++ src/main/java/nettyDemo/TimeServerHandler.java
@@ -1,11 +1,11 @@
 package nettyDemo;
 
-import java.util.Date;
-
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.Attribute;
+import io.netty.util.AttributeKey;
 
 public class TimeServerHandler extends ChannelHandlerAdapter{
 	
@@ -13,6 +13,7 @@ public class TimeServerHandler extends ChannelHandlerAdapter{
 	
 	}
 
+	AttributeKey<Long> loginTime = AttributeKey.valueOf("loginTime");
 	
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
@@ -20,11 +21,30 @@ public class TimeServerHandler extends ChannelHandlerAdapter{
 		byte[] req = new byte[buf.readableBytes()];
 		buf.readBytes(req);
 		String body = new String(req,"UTF-8");
-		System.err.println("Time Server receive order : " + body);
-		String currentTime = "Query time".equalsIgnoreCase(body) ? new Date().toString() : "Bad order";
 		
-		ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+		System.err.println("recieve : " + body);
+		long millis = System.currentTimeMillis();
+		Attribute<Long> attribute = ctx.attr(loginTime);
+		Long loginTime = attribute.get();
+		if(loginTime == null || loginTime == 0){
+			attribute.set(millis);
+		}
+		if(millis - attribute.get() > 15000){
+			ctx.close();
+		}
+//		ByteBuf resp = Unpooled.copiedBuffer(req);
+//		ctx.write(resp);
+		
+		//发送201协议
+		ByteBuf resp = Unpooled.copiedBuffer(new byte[]{2, 0, -55, 0, 0, 0, -50, 123, 34, 111, 119, 110, 73, 110, 102, 111, 34, 58, 123, 34, 103, 111, 108, 100, 34, 58, 49, 48, 48, 48, 48, 44, 34, 119, 101, 105, 122, 104, 105, 34, 58, 49, 44, 34, 105, 109, 103, 34, 58, 34, 104, 116, 116, 112, 115, 58, 47, 47, 115, 101, 114, 46, 99, 112, 109, 114, 106, 46, 99, 111, 109, 47, 116, 111, 117, 120, 105, 97, 110, 103, 47, 49, 52, 48, 47, 49, 49, 52, 48, 46, 112, 110, 103, 34, 44, 34, 108, 111, 103, 105, 110, 73, 112, 34, 58, 34, 49, 48, 46, 48, 46, 49, 46, 49, 55, 50, 34, 44, 34, 110, 97, 109, 101, 34, 58, 34, 106, 107, 102, 103, 115, 107, 106, 34, 44, 34, 105, 115, 79, 110, 108, 105, 110, 101, 34, 58, 116, 114, 117, 101, 44, 34, 117, 115, 101, 114, 73, 100, 34, 58, 34, 49, 34, 44, 34, 112, 108, 97, 121, 101, 114, 73, 100, 34, 58, 49, 49, 52, 48, 125, 44, 34, 105, 115, 79, 112, 101, 110, 34, 58, 102, 97, 108, 115, 101, 44, 34, 105, 115, 83, 117, 99, 99, 101, 115, 115, 34, 58, 116, 114, 117, 101, 125, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
 		ctx.write(resp);
+		
+		
+//		System.err.println("Time Server receive order : " + body);
+//		String currentTime = "Query time".equalsIgnoreCase(body) ? new Date().toString() : "Bad order";
+//		
+//		ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+//		ctx.write(resp);
 	}
 	
 	@Override
--- src/main/java/nettyDemo/TimerServer.java
+++ src/main/java/nettyDemo/TimerServer.java
@@ -2,6 +2,7 @@ package nettyDemo;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -11,7 +12,7 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 public class TimerServer {
 	
 	public static void main(String[] args) throws Exception {
-		int port = 8080;
+		int port = 3625;
 		new TimerServer().bind(port);
 	}
 	
@@ -27,7 +28,7 @@ public class TimerServer {
 			
 			//绑定端口,同步等待成功
 			ChannelFuture f = bootstrap.bind(port).sync();
-			
+			System.err.println("服务器启动成功");
 			//等待服务端监听端口关闭
 			f.channel().closeFuture().sync();
 		}finally{
@@ -39,8 +40,15 @@ public class TimerServer {
 	private class ChildChannelHandler extends ChannelInitializer<SocketChannel>{
 
 		@Override
-		protected void initChannel(SocketChannel arg0) throws Exception {
-			arg0.pipeline().addLast(new TimeServerHandler());
+		protected void initChannel(SocketChannel channel) throws Exception {
+			channel.pipeline().addLast(new TimeServerHandler());
+//			channel.pipeline().addLast(new ReadTimeoutHandler(2));
+		}
+
+		@Override
+		public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+			super.exceptionCaught(ctx, cause);
+			System.err.println(ctx.channel().localAddress().toString() + "断开连接");
 		}
 		
 	}
